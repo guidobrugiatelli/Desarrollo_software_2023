be7a2adb9c1a9d50f493994bb4c4c7ad7c7575e4
Guido Brugiatelli <brugiatelli.guido@gmail.com>
2023-10-10 19:19:38 -0300

    Sumamos ejercicios al código

--- repositorio_clonado/vue-base-example/src/App.vue
+++ repositorio_clonado/vue-base-example/src/App.vue
@@ -5,6 +5,10 @@
       <option value="1">1</option>
       <option value="2">2</option>
       <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+      <option value="6">6</option>
+      <option value="7">7</option>
       <option value="Limpus">Limpus</option>
     </select>
   <div v-if="Ejercicio==1">
@@ -16,6 +20,18 @@
   <div v-if="Ejercicio==3">
     <Ejercicio_3/>
   </div>
+  <div v-if="Ejercicio==4">
+    <Ejercicio_4/>
+  </div>
+  <div v-if="Ejercicio==5">
+    <Ejercicio_5/>
+  </div>
+  <div v-if="Ejercicio==6">
+    <Ejercicio_6/>
+  </div>
+  <div v-if="Ejercicio==7">
+    <Ejercicio_7/>
+  </div>
   <div v-if="Ejercicio=='Limpus'">
     <Limpus/>
   </div>
@@ -25,18 +41,24 @@
 /* eslint-disable */
 
 <script>
-  import ExampleComponent from './components/ExampleComponent'
   import Ejercicio_1 from './components/ejercicio_1.vue'
   import Ejercicio_2 from './components/ejercicio_2.vue'
   import Ejercicio_3 from './components/ejercicio_3.vue'
+  import Ejercicio_4 from './components/Ejercicio4.vue'
+  import Ejercicio_5 from './components/Ejercicio5.vue'
+  import Ejercicio_6 from './components/Ejercicio6.vue'
+  import Ejercicio_7 from './components/Ejercicio7.vue'
   import Limpus from './components/limpus.vue'
   import axios from 'axios'
   export default {
     name: 'App',
     components: {
-      ExampleComponent,
       Limpus,
       Ejercicio_3,
+      Ejercicio_4,
+      Ejercicio_5,
+      Ejercicio_6,
+      Ejercicio_7,
       Ejercicio_2,
       Ejercicio_1
     },
